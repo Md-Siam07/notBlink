3b7419c6d5318caf3a4ed499ad50e0cb92985480
Md. Inzamam-Ul Haque Sobuz <48618774+inzamam-inz@users.noreply.github.com>
2022-04-16 05:23:51 +0600

    update nav-component and user profile

--- FrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Router } from '@angular/router';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,7 +11,12 @@ import { Router } from '@angular/router';
 export class NavBarComponent implements OnInit {
 
   constructor(private userService: UserService, private router: Router) { }
+
   status: boolean = false;
+
+  // background: string = 'red';
+  // fontSize: number = 10;
+
   ngOnInit(): void {
     //this.status = this.userService.loginStatus;
   }
@@ -25,5 +31,17 @@ export class NavBarComponent implements OnInit {
   //   else
   //     this.router.navigateByUrl('login');
   // }
+
+  // change() : string {
+  //   return this.background;
+  //   console.log('red');
+  //   document.documentElement.style.cssText = "background-color: red";
+  // }
+
+  onLogout() {
+    this.userService.deleteToken();
+    this.userService.loginStatus = false;
+    this.router.navigateByUrl('/login');
+  }
 
 }
