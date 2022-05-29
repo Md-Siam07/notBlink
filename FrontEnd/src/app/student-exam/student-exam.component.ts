47abc76483e5bb456a9d8a2c3b52ea400ad2dcad
Md. Inzamam-Ul Haque Sobuz <48618774+inzamam-inz@users.noreply.github.com>
2022-05-30 05:52:02 +0600

    calibration(doing)

--- FrontEnd/src/app/student-exam/student-exam.component.ts
+++ FrontEnd/src/app/student-exam/student-exam.component.ts
@@ -8,11 +8,25 @@ import { StudentExamService } from '../shared/student-exam.service';
   styleUrls: ['./student-exam.component.css']
 })
 export class StudentExamComponent implements OnInit {
+  calibrationDone:Boolean = false;
 
   constructor(private examService: StudentExamService) { }
   examDetails = new Exam();
   ngOnInit(): void {
     this.examDetails = this.examService.selectedExam;
+  }
+
+  clickCount:number[] = [0,0,0,0,0,0,0,0,0];
+  clickDoneCount:number = 0;
+
+  calibrationClick(id:number) {
+    this.clickCount[id]++;
+    if (this.clickCount[id] == 5)    this.clickDoneCount++;
+
+    if (this.clickDoneCount == 9) {
+      //alert("Calibration Done");
+      this.calibrationDone = true;
+    }
   }
 
 }
