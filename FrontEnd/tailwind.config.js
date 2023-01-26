5be7300fb81432a5770a9ca698767790a776d367
Nazmus Sakib Ahmed <bsse1108@iit.du.ac.bd>
2023-01-26 21:53:08 +0600

    login new

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "./src/app/features/*.{html,ts}",
     "./src/app/student-exam-card/*.{html,ts}",
     "./src/app/sign-up/*.{html,ts}",
-    "./src/app/login/*.{html,ts}"
+    "./src/app/login/*.{html,ts}",
+    "./src/app/nav-bar/*.{html,ts}"
     
   ],
   theme: {
